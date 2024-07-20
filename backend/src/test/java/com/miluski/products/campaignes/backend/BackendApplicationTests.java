9c8a39d82a32afd87ea19e496f866fa11f9941ca
miluski <maksymilian.sowula@gmail.com>
2024-07-20 10:50:43 +0200

    feat: Initialize project structure

---
+++ backend/src/test/java/com/miluski/products/campaignes/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miluski.products.campaignes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
