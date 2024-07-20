d956bbf132813b7cdf478a9d87f449faa240bcba
miluski <maksymilian.sowula@gmail.com>
2024-07-20 21:09:41 +0200

    feat: Implemented backend functionallities of login ability to seller account

---
+++ backend/src/main/java/com/miluski/products/campaignes/backend/model/JwtRequestFilter.java
@@ -0,0 +1,56 @@
+package com.miluski.products.campaignes.backend.model;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.miluski.products.campaignes.backend.model.services.JwtTokenService;
+import com.miluski.products.campaignes.backend.model.services.UserDetailsServiceImpl;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    public JwtRequestFilter(JwtTokenService jwtTokenService, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.jwtTokenService = jwtTokenService;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        if (requestURI.equals("/api/auth/user/login") || requestURI.equals("/api/auth/user/logout")
+                || requestURI.equals("/api/auth/refresh/tokens")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = jwtTokenService.getTokenFromCookies(request);
+        if (token != null) {
+            String username = jwtTokenService.getUsername(token);
+            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
+            if (jwtTokenService.validateToken(token, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
---
+++ backend/src/main/java/com/miluski/products/campaignes/backend/model/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.miluski.products.campaignes.backend.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import com.miluski.products.campaignes.backend.model.services.JwtTokenService;
+import com.miluski.products.campaignes.backend.model.services.UserDetailsServiceImpl;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtTokenService jwtTokenService;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    public SecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl, JwtTokenService jwtTokenService) {
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf((csrf) -> csrf.disable())
+                .headers((headers) -> headers
+                        .frameOptions((frameOptions) -> frameOptions.disable()))
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                        .maximumSessions(1))
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers("/api/auth/user/login").permitAll()
+                                .requestMatchers("/api/auth/user/logout").permitAll()
+                                .requestMatchers("/api/auth/tokens/refresh").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .anyRequest().authenticated())
+                .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(
+                        ((request, response, exception) -> {
+                            System.out.println(exception.getMessage());
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                        })))
+                .addFilterBefore(new JwtRequestFilter(jwtTokenService, userDetailsServiceImpl),
+                        UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        Argon2PasswordEncoder argon2PasswordEncoder = new Argon2PasswordEncoder(16, 32, 1, 60000, 10);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsServiceImpl);
+        daoAuthenticationProvider.setPasswordEncoder(argon2PasswordEncoder);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+}
