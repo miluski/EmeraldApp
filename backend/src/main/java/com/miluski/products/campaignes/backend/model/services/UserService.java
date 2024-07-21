a192312518713ac6b0ac7ac5483c30e3fdd8b0a6
miluski <maksymilian.sowula@gmail.com>
2024-07-21 18:21:14 +0200

    fix: Removed / from cookie domain

--- backend/src/main/java/com/miluski/products/campaignes/backend/model/services/UserService.java
+++ backend/src/main/java/com/miluski/products/campaignes/backend/model/services/UserService.java
@@ -105,7 +105,7 @@ public class UserService {
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
-        cookie.setDomain("emerald-app-88c863e81f66.herokuapp.com/");
+        cookie.setDomain("emerald-app-88c863e81f66.herokuapp.com");
         return cookie;
     }
 
